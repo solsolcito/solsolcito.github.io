103070d64bbba987a80f9031304b66048fc0955b
solange alarcon <solealarconmelinao@gmail.com>
2022-11-11 16:28:22 -0300

    pagina

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Solange Andrea Alarcón 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
