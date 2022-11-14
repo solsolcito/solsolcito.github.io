f164509e6556f67cb2b8b901fc66f6afc4796be1
solange alarcon <solealarconmelinao@gmail.com>
2022-11-14 10:58:51 -0300

    segunda clase de mi pagina

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Solange Andrea Alarcón 
-        </p>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img src="mifoto.pg.jpg" className="sol" alt="..." />
+        <div className="card-body">
+          <h1>Solange Alarcón Melinao</h1>
+          <h5 className="card-title">Mi Perfil</h5>
+          <p className="card-text">hola.</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+        <div className="card-body">
+          <a href="#" className="card-link">Card link</a>
+          <a href="#" className="card-link">Another link</a>
+        </div>
+      </div>
+
     </div>
   );
 }
